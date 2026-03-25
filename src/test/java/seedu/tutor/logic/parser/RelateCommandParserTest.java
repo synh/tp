743d5df4f8e2be92bfdc5efbedec811c0ea164ee
LinqWithQ <linqherng2005@gmail.com>
2026-03-25 16:32:08 +0800

    Improve variable naming

--- src/test/java/seedu/tutor/logic/parser/RelateCommandParserTest.java
+++ src/test/java/seedu/tutor/logic/parser/RelateCommandParserTest.java
@@ -9,53 +9,53 @@ import seedu.tutor.logic.parser.exceptions.ParseException;
 public class RelateCommandParserTest {
 
     private final RelateCommandParser parser = new RelateCommandParser();
-    private final String[] validInput = new String[] {" a\\test/test/test/test", " a\\1/1/1/1 a\\2/2/2/2",
+    private final String[] validFormat = new String[] {" a\\test/test/test/test", " a\\1/1/1/1 a\\2/2/2/2",
         "  d\\3/3/3/3", " d\\4/4/4/4 d\\5/5/5/5 a\\6/6/6/6",
         "    a\\ a  dqwd asd/ awd wd cawf / awefaw  awefsdfweg/sdfw wef    d\\sfwgrtb/  rg wrv/rgfr gree/erfe"};
-    private final String[] invalidInput = new String[] {" a\\", " d\\", " a\\1", " d\\2/2", " a\\3/3/3",
+    private final String[] invalidFormat = new String[] {" a\\", " d\\", " a\\1", " d\\2/2", " a\\3/3/3",
         " d\\5/5/5/5/5", " na\\test/test/test/test"};
     private final String[] extraArgs = new String[] {" n/bruh a\\test/test/test/tes", " a\\1/1/1/1 e/ a\\2/2/2/2",
         " d\\4/4/4/4 d\\5/5/5/5 p/0123456 a\\6/6/6/6"};
 
     @Test
-    public void parse_validInput_success() {
-        int expect = validInput.length;
-        int test = validInput.length;
-        for (String input: validInput) {
+    public void parse_validFormatInput_success() {
+        int expectedExceptionsAmount = 0;
+        int actualExceptionsAmount = 0;
+        for (String input: validFormat) {
             try {
                 parser.parse(input);
             } catch (ParseException e) {
-                test--;
+                actualExceptionsAmount++;
             }
         }
-        assertEquals(expect, test);
+        assertEquals(expectedExceptionsAmount, actualExceptionsAmount);
     }
 
     @Test
-    public void parse_invalidInputFormat_failure() {
-        int expect = invalidInput.length;
-        int test = 0;
-        for (String input: invalidInput) {
+    public void parse_invalidFormatInput_failure() {
+        int expectedExceptionAmount = invalidFormat.length;
+        int actualExceptionAmount = 0;
+        for (String input: invalidFormat) {
             try {
                 parser.parse(input);
             } catch (ParseException e) {
-                test++;
+                actualExceptionAmount++;
             }
         }
-        assertEquals(expect, test);
+        assertEquals(expectedExceptionAmount, actualExceptionAmount);
     }
 
     @Test
     public void parse_extraArgsInput_failure() {
-        int expect = extraArgs.length;
-        int test = 0;
+        int expectedExceptionAmount = extraArgs.length;
+        int actualExceptionAmount = 0;
         for (String input: extraArgs) {
             try {
                 parser.parse(input);
             } catch (ParseException e) {
-                test++;
+                actualExceptionAmount++;
             }
         }
-        assertEquals(expect, test);
+        assertEquals(expectedExceptionAmount, actualExceptionAmount);
     }
 }
